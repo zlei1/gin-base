473536da542ac7ac71fa61b637fa4ff068b578f5
zlei1 <zhenglei123a@gmail.com>
2020-08-10 22:38:04 +0800

    global app

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	"gin-base/config"
+	"gin-base/pkg/global"
+	"gin-base/pkg/log"
 	"gin-base/routes"
 )
 
@@ -15,8 +20,23 @@ func main() {
 	pflag.Parse()
 
 	conf := config.Setup(*cfgPath)
-	app := Setup(conf)
+	app := global.Setup(conf)
 	defer app.DB.Close()
+
+	logConfig := log.Config{
+		Writers:    viper.GetString("log.writers"),
+		Level:      viper.GetString("log.level"),
+		File:       viper.GetString("log.file"),
+		WarnFile:   viper.GetString("log.warn_file"),
+		ErrorFile:  viper.GetString("log.error_file"),
+		MaxSize:    viper.GetInt("log.max_size"),
+		MaxBackups: viper.GetInt("log.max_backups"),
+		MaxAge:     viper.GetInt("log.max_age"),
+	}
+	err := log.New(&logConfig, log.InstanceZapLogger)
+	if err != nil {
+		fmt.Printf("setup log err: %v", err)
+	}
 
 	router := routes.Setup()
 	router.Run()
--- pkg/global/app.go
+++ pkg/global/app.go
@@ -1,15 +1,12 @@
-package main
+package global
 
 import (
-	"fmt"
-
 	"github.com/go-redis/redis/v8"
 	"github.com/jinzhu/gorm"
 	"github.com/spf13/viper"
 
 	"gin-base/app/models"
 	"gin-base/config"
-	"gin-base/pkg/log"
 	pkg_redis "gin-base/pkg/redis"
 )
 
@@ -35,28 +32,9 @@ func Setup(cfg *config.Config) *Application {
 
 	app.RedisClient = pkg_redis.Setup()
 
-	SetupLog()
-
 	if viper.GetString("run_mode") == ModeDebug {
 		app.Debug = true
 	}
 
 	return app
-}
-
-func SetupLog() {
-	config := log.Config{
-		Writers:    viper.GetString("log.writers"),
-		Level:      viper.GetString("log.level"),
-		File:       viper.GetString("log.file"),
-		WarnFile:   viper.GetString("log.warn_file"),
-		ErrorFile:  viper.GetString("log.error_file"),
-		MaxSize:    viper.GetInt("log.max_size"),
-		MaxBackups: viper.GetInt("log.max_backups"),
-		MaxAge:     viper.GetInt("log.max_age"),
-	}
-	err := log.New(&config, log.InstanceZapLogger)
-	if err != nil {
-		fmt.Printf("setup log err: %v", err)
-	}
 }
